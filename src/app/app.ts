d75e616ff2debbd0d4e684c18b38ba7e3d7f29f9
shalimov <shalimov@profiterole.group>
2025-12-11 16:55:39 +0300

    upd

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,22 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "./components/header/header";
+import { Hero } from "./components/hero/hero";
+import { Rates } from "./components/rates/rates";
+import { Categories } from "./components/categories/categories";
+import { Blog } from "./components/blog/blog";
+import { Footer } from "./components/footer/footer";
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  //RouterOutlet
+  imports: [CommonModule, Header, Hero, Rates, Categories, Blog, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('calc-home-iq');
+  //protected readonly title = signal('calc-home-iq');  
+  title = 'HomeIQ';
 }
